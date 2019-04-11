07f74798f00d582050e8e4e78e7698eccee29718
FlandreDaisuki <vbnm123c@gmail.com>
2019-04-11 22:45:24 +0800

    Add integral test cases

--- test/test.js
+++ test/test.js
@@ -1,15 +1,38 @@
 import metablock from '../src/index';
 import { rollup } from 'rollup';
-import assert from 'assert';
-import { readFileSync } from 'fs';
+import { readFileSync, readdirSync } from 'fs';
+import { strictEqual } from 'assert';
 
-process.chdir(__dirname);
+const doIntegral = async(path) => {
+  process.chdir(path);
 
-describe('intregral', () => {
-  it('aaa', () => true);
+  const options = JSON.parse(readFileSync('./options.json', 'utf-8'));
+  const answer = readFileSync('./answer.txt', 'utf-8');
+
+  const bundle = await rollup({
+    input: `${__dirname}/main.js`,
+    plugins: [metablock(options)],
+  });
+
+  const bundleOut = await bundle.generate({
+    output: 'out.js',
+    format: 'esm',
+  });
+
+  strictEqual(answer, bundleOut.output[0].code);
+
+  process.chdir(__dirname);
+};
+
+describe('integral', () => {
+  process.chdir(__dirname);
+
+  const casedirs = readdirSync('testcase');
+
+  for (const dir of casedirs) {
+    it(`${dir}`, () => doIntegral(`${__dirname}/testcase/${dir}`));
+  }
 });
-
-
 
 // describe('rollup-plugin-userscript-metablock', () => {
 
