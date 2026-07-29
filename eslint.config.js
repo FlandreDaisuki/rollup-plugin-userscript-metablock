126464ac423b70030f41328ec353843f727672fc
FlandreDaisuki <github@flandre.tw>
2026-07-29 20:30:55 +0800

    build: update eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import flandre from '@flandredaisuki/eslint-config';
+
+/** @satisfies {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'examples/**/out.user.js',
+      'test/options/file/bads/**',
+    ],
+  },
+  ...flandre.preset,
+  {
+    files: [
+      'examples/**/*.js',
+      'test/bundle/main.js',
+    ],
+    rules: {
+      'no-console': 'off',
+    },
+  },
+];
