62078bf14ab37a120afb84c7ef65f95cef2c2336
FlandreDaisuki <github@flandre.tw>
2026-07-29 21:00:51 +0800

    ci: automate npm publishing from tags

--- npm-scripts/publish.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import { spawnSync } from 'child_process';
-import path from 'path';
-
-const packageJson = fs.readFileSync(path.resolve(import.meta.dirname, '../package.json'), 'utf8');
-const pkg = JSON.parse(packageJson);
-spawnSync('git', ['tag', `v${pkg.version}`]);
